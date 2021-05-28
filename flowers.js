9c5f32c8ec74732bf23f32e7146af145ad9ff454
eschnei2 <everettschneider@gmail.com>
2021-05-28 15:38:17 -0500

    Success!

--- flowers.js
+++ flowers.js
@@ -1,19 +1,26 @@
 const flowers = [
-
+    {
+        id: 1,
+        color: "white",
+        species: "Rose",
+        price: .90
+    },
+    {
+        id: 2,
+        color: "Red",
+        species: "Tulip",
+        price: 1.10
+    }
 ]
 
 const addFlower = (flowerObject) => {
-    /*
-        This function should add an `id` property
-        to the object that was sent to the function.
-        The value of the `id` property should be 1
-        greater than the current maximum `id` in the
-        array.
+    const lastFlower = flowers.length -1
+    const currentLastFlower = flowers[lastFlower]
+    const maxId = currentLastFlower.id
+    const idForNewNote = maxId +1
 
-        Once the `id` property has been added to the,
-        object, the object should then be put into the
-        `flowers` array.
-    */
+    flowerObject.id = idForNewNote
+    flowers.push(flowerObject)
 
 
 }
@@ -29,15 +36,16 @@ const findExpensiveFlowers = () => {
         greater than or equal to 1.00, it should be
         added to the `expensiveFlowers` array.
     */
-
-
-
+   for (const flower of flowers) {
+       if (flower.price >= 1.00){
+          expensiveFlowers.push(flower)
+       } 
+   }
 
     return expensiveFlowers  // Do not change this code
 }
 
-
-
+/* console.log(expensiveFlowers) */
 
 // Do not touch this code
 module.exports = {
